3220d84ad7db6e8fd2e52a3ab8ef7896e01dd7d5
Dadadon <davianberoni@yahoo.com>
2022-08-11 07:34:23 -0400

    add modules

---
+++ src/modules/listItem.js
@@ -0,0 +1,31 @@
+const todoList = JSON.parse(localStorage.getItem("todoList")) || [];
+const form = document.forms["todoForm"];
+
+export default class listItem {
+  constructor(description, status, position) {
+    this.description = description;
+    this.status = status;
+    this.position = position;
+  }
+
+    deleteItem(index) {
+        todoList.splice(index, 1);
+        localStorage.setItem('todoList', JSON.stringify(todoList));
+    }
+    
+    addItem() {
+        const descr = form.addTodo;
+        const pos = todoList.length;
+        const stat = 'not checked';
+        const item = new listItem(descr.value, stat, pos);
+        todoList.push(item);
+
+        localStorage.setItem("todoList", JSON.stringify(todoList));
+        document.forms['todoForm'].reset();
+    }
+
+    setStatus(index, stat) {
+        todoList[index].status = stat;
+        localStorage.setItem("todoList", JSON.stringify(todoList));
+    }
+}
